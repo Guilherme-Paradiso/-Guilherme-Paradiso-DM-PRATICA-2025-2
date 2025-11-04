846d11cf5d6d8bc85cdadbaa4ea0fddfcda48545
Guilherme-Paradiso <guiparadiso2006@gmail.com>
2025-11-04 07:59:35 -0300

    Parte 2 - Prática 3: Lista simples.

--- app/src/main/java/com/weatherapp/ListActivity.kt
+++ app/src/main/java/com/weatherapp/ListActivity.kt
@@ -1,25 +1,37 @@
 package com.weatherapp
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.weatherapp.model.City
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class ListActivity : ComponentActivity() {
@@ -36,21 +48,58 @@ class ListActivity : ComponentActivity() {
     }
 }
 
+private fun getCities() = List(20) { i ->
+    City(name = "Cidade $i", weather = "Carregando clima...")
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ListPage(modifier: Modifier = Modifier) {
-        Column(
-            modifier = modifier.fillMaxSize()
-                .background(Color.Red)
-                .wrapContentSize(Alignment.Center)
-        ) {
-            Text(
-                text = "Favoritas",
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                modifier = modifier.align(CenterHorizontally),
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp
-            )
+    val cityList = remember { getCities().toMutableStateList() }
+    val activity = LocalActivity.current as Activity // Para os Toasts
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        items(cityList, key = { it.name }) { city ->
+            CityItem(city = city, onClose = {
+/* TO DO */
+            }, onClick = {
+/* TO DO */
+            })
         }
     }
+}
+
+@Composable
+fun CityItem(
+    city: City,
+    onClick: () -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
+        )
+        Spacer(modifier = Modifier.size(12.dp))
+        Column(modifier = modifier.weight(1f)) {
+            Text(modifier = Modifier,
+                text = city.name,
+                fontSize = 24.sp)
+            Text(modifier = Modifier,
+                text = city.weather?:"Carregando clima...",
+
+                fontSize = 16.sp)
+
+        }
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
