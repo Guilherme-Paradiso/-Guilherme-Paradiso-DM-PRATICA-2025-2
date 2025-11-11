7b1c95fecb84fe201b5af3a28207de20385db48a
Guilherme-Paradiso <guiparadiso2006@gmail.com>
2025-11-11 10:07:30 -0300

    Prática 4 - finalizada

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -1,13 +1,22 @@
 package com.weatherapp.ui
 
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.weatherapp.model.MainViewModel
 
 @Composable
@@ -17,8 +26,32 @@ fun MapPage(modifier: Modifier = Modifier,
     val recife = LatLng(-8.05, -34.9)
     val caruaru = LatLng(-8.27, -35.98)
     val joaopessoa = LatLng(-7.12, -34.84)
+    val camPosState = rememberCameraPositionState ()
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
 
-    GoogleMap(modifier = Modifier.fillMaxSize()) {
+
+
+    GoogleMap(modifier = Modifier.fillMaxSize(), onMapClick = {
+        viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it) },
+        cameraPositionState = camPosState,
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
+    ) {
+
+
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                Marker( state = MarkerState(position = it.location),
+                    title = it.name, snippet = "${it.location}")
+            }
+        }
 
         Marker(
             state = MarkerState(position = recife),
@@ -38,7 +71,7 @@ fun MapPage(modifier: Modifier = Modifier,
             state = MarkerState(position = joaopessoa),
             title = "João Pessoa",
             snippet = "Marcador em João Pessoa",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
         )
 
     }
